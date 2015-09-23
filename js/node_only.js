63c1a056616e8503180cbd5a8ef3b39b9ef19b6a
Amy <akfuent@gmail.com>
2015-09-22 22:30:10 -0500

    website help

--- js/node_only.js
+++ js/node_only.js
@@ -2,3 +2,26 @@
    Node only scripts - Only to be used via node command line tool
    				     - this file is NOT included in index.html
    ========================================================================== */
+
+
+var charname = "Aragon";
+
+console.log("charname");
+
+level = "Fighter_1",
+background = "Noble",
+player_name= "Amy",
+faction = "Forest",
+race = "Elf",
+alignment = "Lawful neutral";
+
+var character_information = 'Hi I am ' + charname + ' and I am a ' + level; 
+console.log(character_information); 
+
+var expoints = "500",
+dci_number = "1000",
+strength = "+4",
+dexterity = "1",
+constitution = "+2",
+intelligence = "4",
+charisma = "2";
